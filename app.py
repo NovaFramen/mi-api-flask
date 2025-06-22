6dfc88ef76e7cba6c2547e43f59303c074671a2f
Mar <m93070103@gmail.com>
2025-06-22 16:28:28 -0400

    Agregar base y ruta raíz para mostrarlo

--- app.py
+++ app.py
@@ -1,15 +1,59 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, request, jsonify, render_template
+from supabase import create_client, Client
 import psycopg2
+import os
 
 
 app = Flask(__name__)
 
+
+
 DATABASE_URL = "postgresql://komicuser:yJh4z95SKQh9ti71MK1bwd4zekbg4ZVz@dpg-d1bkdt8dl3ps73epp700-a.oregon-postgres.render.com/komicdb"
+
+
+SUPABASE_URL = "https://jeboojuntugrognjvlzc.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplYm9vanVudHVncm9nbmp2bHpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MTk0MjMsImV4cCI6MjA2NjE5NTQyM30.iLQBgkRnXWZYljoSvkvaFLZC8RIaIqZxiziirniLeJQ" 
+BUCKET_NAME = "archivos"  
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL)
     return conn
+
+
+
+
+@app.route('/subir', methods=['POST'])
+def subir_archivo():
+    if 'archivo' not in request.files:
+        return jsonify({"error": "No se envió ningún archivo"}), 400
+
+    archivo = request.files['archivo']
+    nombre_archivo = archivo.filename
+
+    contenido = archivo.read()
+
+    try:
+        resultado = supabase.storage.from_(BUCKET_NAME).upload(nombre_archivo, contenido, {"content-type": archivo.mimetype})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    url = f"{SUPABASE_URL}/storage/v1/object/public/{BUCKET_NAME}/{nombre_archivo}"
+
+    return jsonify({"mensaje": "Archivo subido correctamente", "url": url})
+
+
+
+@app.route('/form-subir')
+def form_subir():
+    return render_template('formulario_subida.html')
+
+
+
+
 
 @app.route('/')
 def home():
@@ -142,6 +186,19 @@ def listar_usuarios():
 
 
 
+
+
+
+@app.route('/ver_imagen')
+def ver_imagen():
+    url = "https://jeboojuntugrognjvlzc.supabase.co/storage/v1/object/public/archivos/gato.jpg"
+    return render_template('mostrar.html', url=url)
+
+
+@app.route('/ver_pdf')
+def ver_pdf():
+    url = "https://jeboojuntugrognjvlzc.supabase.co/storage/v1/object/public/archivos/gato.pdf"
+    return render_template('mostrarpdf.html', url=url)
 
 
 
